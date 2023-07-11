bbab55dedde732bb25cf1c09068927e306d86b39
jenishsavaliya <jenishsavaliya4022@gmail.com>
2023-07-11 11:35:50 +0530

    add a car catalogue section

--- components/index.tsx
+++ components/index.tsx
@@ -1,6 +1,17 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
